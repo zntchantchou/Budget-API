6b36ff51246d9ea3ddad172ed0d40ae0cef9bd09
z.ntchantchou <z.ntchantchou@technologyandstrategy.com>
2021-09-20 15:05:49 +0200

    adds appsettings.json to gitignore / deletes template files

--- API/Startup.cs
+++ API/Startup.cs
@@ -9,49 +9,48 @@ using Microsoft.OpenApi.Models;
 
 namespace API
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration config)
     {
-        public Startup(IConfiguration config)
-        {
-            _config = config;
-        }
-
-        public IConfiguration _config { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-          services.AddDbContext<DataContext>(options => {
-            options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-          });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      _config = config;
     }
+
+    public IConfiguration _config { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+      });
+      services.AddDbContext<DataContext>(options =>
+      {
+        options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+      }
+
+      app.UseHttpsRedirection();
+      app.UseRouting();
+      app.UseCors(policy => policy.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:4200"));
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
